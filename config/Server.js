e281145657756bb2f8adc1a8c4d9b8de3aff1622
thiagoleitedev <thiagoleitebr@gmail.com>
2020-02-15 11:37:15 -0300

    Updates

--- config/Server.js
+++ config/Server.js
@@ -1,4 +1,4 @@
-//bibliotecas 
+//bibliotecas
 let express = require('express');
 let body_parser = require('body-parser');
 let cors = require('cors');
@@ -9,43 +9,38 @@ let app = express();
 /** Processamento de Contente type
  * application/json e x-www-form-url-encoded
  */
-app.use(body_parser.urlencoded({extended:true}))
-app.use(body_parser.json())
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json());
 
 /**
- * Configuração de CORS 
+ * Configuração de CORS
  */
 app.use(cors());
 
 /**
- * Referencia e configuração de rotas 
+ * Referencia e configuração de rotas
  */
- const teste_router = require('../routes/TesteRouter')
- const auth_router = require('../routes/AuthRouter')
+// const teste_router = require('../routes/TesteRouter');
+// const auth_router = require('../routes/AuthRouter');
 
-function initDB() {
-  await sequelize.sync();
-  const jane = await User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20),
-  });
-  console.log(jane.toJSON());
-}
+// Temporada.hasOne(User);
 
-initDB();
- 
+/** Registro das rotas */
+// app.use('/auth', auth_router);
+// app.use('/testes', teste_router);
 
-Temporada.hasOne(User);
+app.get('/api/users', async (req, res) => {
+  const { User } = require('../models');
 
- /** Registro das rotas */
- app.use("/auth",auth_router)
- app.use("/testes",teste_router)
+  const users = await User.findAll().then();
 
- /**
-  * Ativar Passport e Middleware de Rotas JWT
+  res.json(users);
+});
+
+/**
+ * Ativar Passport e Middleware de Rotas JWT
  */
- const auth = require("../config/AuthJwt")();
- app.use(auth.initialize())
+// const auth = require('../config/AuthJwt')();
+// app.use(auth.initialize());
 
- module.exports = app;
-
+module.exports = app;
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,18 +1,12 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
-
-class User extends Model {}
-User.init(
-  {
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
     },
     name: DataTypes.STRING,
-    weightStart: DataTypes.INTEGER,
-    weightEnd: DataTypes.INTER,
-    isActive: DataTypes.BOOLEAN,
-  },
-  { sequelize, modelName: 'user' },
-);
+  });
+
+  return User;
+};
